c14c263705fb0ee838ed9a0f18663046efbdbb4e
Bjarki Sörens <bjerkins8@gmail.com>
2013-06-01 18:11:50 +0000

    Added socket.io and twitter dependencies.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require("express");
+var twitter = require("twitter");
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server, {log: false});
+
 app.use(express.logger());
 
 app.configure(function(){
@@ -13,11 +17,26 @@ app.configure(function(){
     app.use(app.router);
 });
 
+var twit = new twitter({
+    consumer_key: 'GZqfJsBQM80uRyuGtVcTkw',
+    consumer_secret: 'WqpulpVjUN0WAzejAFzwRiqXoSm98ksYYpe2vPusQ',
+    access_token_key: '47104104-lnUI4JZoaQPlsXo3soZNnnnz14pqcwYUzaD9HiDHt',
+    access_token_secret: 'gOwqsLIiybsyq9AY8jP8lRZJ7inr3oLN1aUYmZwTJ8A'
+});
+
+twit.stream('statuses/filter', { track: 'Tommi TwoTimes' }, function(stream) {
+    console.log('listening for keywords');
+    stream.on('data', function (data) {
+        io.sockets.emit('tweet', data.text);
+        console.log('i sent a tweet to the client');
+    });
+});
+
 app.get('/', function(request, response) {
-  response.redirect('/index.html');
+    response.redirect('/index.html');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+server.listen(port, function() {
+    console.log("Listening on " + port);
 });
