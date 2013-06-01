b0a97a03de41293809bc014cfc808014ed981c4b
Tindur <tindur.h@gmail.com>
2013-06-01 15:51:59 +0000

    index.html2

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({
+        dumpExceptions: true, 
+        showStack: true
+    }));
+    app.use(app.router);
+});
+
 app.get('/', function(request, response) {
   response.redirect('/index.html');
 });
